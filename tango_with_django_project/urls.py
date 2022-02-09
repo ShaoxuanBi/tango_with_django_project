30133b0fe9c7e314b78d690f4e5415585135716b
“FiShaoxuan <2551980B@student.gla.ac.uk>
2022-02-09 21:05:48 +0000

    chapter 3

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -21,7 +21,7 @@ from rango import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('rango/', include('rango.urls')),
+    path('rango/', include('rango.urls')), # whole app name is rango
     # The above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
 ]
